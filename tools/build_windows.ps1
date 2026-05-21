8689c1e7c5b94e8c506d2ae30e68c4f848d32a37
fwfx1234 <864498233@qq.com>
2026-05-21 19:37:00 +0800

    fix: update build script to use tools/py_desktop_tools.spec

--- tools/build_windows.ps1
+++ tools/build_windows.ps1
@@ -1,4 +1,4 @@
 $ErrorActionPreference = "Stop"
 Set-Location (Join-Path $PSScriptRoot "..")
 uv sync --group build
-uv run pyinstaller build\pyinstaller\py_desktop_tools.spec --noconfirm
+uv run pyinstaller tools\py_desktop_tools.spec --noconfirm
